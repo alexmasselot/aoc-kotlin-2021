977a4e7ca3a6aa281f4ec6e48863482e6be7df79
Alexandre Masselot <alexandre.masselot@gmail.com>
2021-12-02 11:30:43 +0100

    day 1 A

---
+++ src/day01/day01A.kt
@@ -0,0 +1,18 @@
+package day01
+
+import java.io.File
+
+/**
+@author Alexandre Masselot
+@Copyright L'Occitane 2021
+ */
+fun main() {
+    val filename = "src/day01/depths.txt"
+    val n = File(filename).readLines()
+        .map { it.toInt() }
+        .zipWithNext()
+        .filter { (a, b) -> a < b }
+        .count()
+
+    println(n)
+}
